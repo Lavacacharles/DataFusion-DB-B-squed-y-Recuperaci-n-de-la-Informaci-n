7eaa4e575b3eef6818cdc186a2b7027df8b1caaf
Claudio Echarre <claudio.echarre@utec.edu.pe>
2024-11-13 18:13:59 -0500

    Update index.js

--- front/pages/index.js
+++ front/pages/index.js
@@ -7,7 +7,11 @@ export default function HomePage() {
   const [inputText, setInputText] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [tableData, setTableData] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('postgres');
+  const [executionTime, setExecutionTime] = useState(null);
+  const [showInfoPopup, setShowInfoPopup] = useState(false);
 
+  
 
   const parseCSV = (csvText) => {
     const rows = csvText.trim().split('\n');
@@ -18,7 +22,6 @@ export default function HomePage() {
     return data.map(row => row.join(',')).join('\n');
   };
 
-
   const handleSearch =  () => {
    
     if (!inputText.trim().endsWith(';')) {
@@ -28,14 +31,17 @@ export default function HomePage() {
 
     setErrorMessage('');
     console.log("Consulta válida:", inputText);
-    const csvResponse = 'name,age,city,occupation,salary,department\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation';
+    const csvResponse = 'name,age,city,occupation,salary,department\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation\nJohn,30,New York,Engineer,70000,R&D\nJane,25,Boston,Doctor,85000,Health\nDoe,22,San Francisco,Artist,50000,Art\nAlice,29,Chicago,Teacher,60000,Education\nBob,34,Seattle,Nurse,55000,Health\nCharlie,28,Austin,Architect,75000,Construction\nDiana,40,Denver,Scientist,95000,Research\nEve,27,Miami,Lawyer,67000,Law\nFrank,26,Orlando,Chef,52000,Hospitality\nGrace,32,Dallas,Pilot,88000,Aviation';
     try {
+      const startTime = performance.now();
       /*
-      const response = await axios.post('http://localhost:5000/api/search', { query: inputText }, {
+      const response = await axios.post('http://localhost:5000/api/search', { query: inputText, index: selectedOption}, {
         headers: { 'Content-Type': 'application/json' },
       });
       //const csvData = response.data;
       //const data = parseCSV(csvData);*/
+      const endTime = performance.now(); // Tiempo de fin
+      setExecutionTime((endTime - startTime).toFixed(2));
       const data = parseCSV(csvResponse);
       setTableData(data);
     } catch (error) {
@@ -53,6 +59,14 @@ export default function HomePage() {
     setInputText('');
     setErrorMessage('');
     setTableData([]);
+    setExecutionTime(null);
+  };
+  
+  const handleInfoClick = () => {
+    setShowInfoPopup(true);
+  };
+  const handleClosePopup = () => {
+    setShowInfoPopup(false);
   };
 
   const handleDownloadCSV = () => {
@@ -69,6 +83,10 @@ export default function HomePage() {
     URL.revokeObjectURL(url);
   };
 
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -78,47 +96,85 @@ export default function HomePage() {
       </header>
 
       <main className={styles.main}>
-      <div className={styles.textAreaWrapper}>
+        <div className={styles.buttonContainer}>
+          <button
+            className={`${styles.optionButton} ${selectedOption === 'postgres' ? styles.active : ''}`}
+            onClick={() => handleOptionChange('postgres')}
+          >
+            Postgres
+          </button>
+          <button
+            className={`${styles.optionButton} ${selectedOption === 'own' ? styles.active : ''}`}
+            onClick={() => handleOptionChange('own')}
+          >
+            Own Implementation
+          </button>
+        </div>
+        <div className={styles.textAreaWrapper}>
           <textarea
             className={styles.textArea}
             placeholder="Enter your query ..."
             value={inputText}
             onChange={(e) => setInputText(e.target.value)}
           />
-
-          <button className={styles.clearButton} onClick={handleClear}>
-            <img src="/trash-icon.png" alt="Clear" className={styles.clearIcon} />
-          </button>
+          <div className={styles.buttonGroup}>
+            <button className={styles.clearButton} onClick={() => setInputText('')}>
+              <img src="/trash-icon.png" alt="Clear" className={styles.clearIcon} />
+            </button>
+            <button className={styles.infoButton} onClick={handleInfoClick}>
+              i
+            </button>
+          </div>
         </div>
 
         {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
 
+        {showInfoPopup && (
+          <div className={styles.popupOverlay} onClick={handleClosePopup}>
+            <div className={styles.popupContent} onClick={(e) => e.stopPropagation()}>
+              <h2 className={styles.popupTitle}>Información</h2>
+              <p>Ingrese una consulta como el siguiente ejemplo:</p>
+              <p className={styles.emptyParagraph}></p>
+              <p>SELECT title, artista, lyric FROM Audio WHERE lyric @@ 'amor en tiempos de guerra' LIMIT 10;</p>
+              <button onClick={handleClosePopup} className={styles.closePopupButton}>Cerrar</button>
+            </div>
+          </div>
+        )}
+
+
         <button className={styles.searchButton} onClick={handleSearch}>
           Search
         </button>
+        {executionTime && (
+          <p className={styles.executionTime}>
+            Tiempo de ejecución: {executionTime} ms
+          </p>
+        )}
 
         {tableData.length > 0 && (
-        <div className={styles.tableContainer}>
-          <table className={styles.table}>
-            <thead>
-              <tr>
-                {tableData[0].map((header, index) => (
-                  <th key={index}>{header}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.slice(1).map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  {row.map((cell, cellIndex) => (
-                    <td key={cellIndex}>{cell}</td>
+          <div className={styles.tableContainer}>
+            <div className={styles.scrollableTable}>
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    {tableData[0].map((header, index) => (
+                      <th key={index}>{header}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableData.slice(1).map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                      {row.map((cell, cellIndex) => (
+                        <td key={cellIndex}>{cell}</td>
+                      ))}
+                    </tr>
                   ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        )}
 
       <button className={styles.downloadButton} onClick={handleDownloadCSV}>
               Descargar CSV
